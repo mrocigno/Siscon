16888accd861d013d6da2ae7dff194b971620788
Matheus <rocignom@gmail.com>
2019-05-01 20:00:54 -0300

    create report

--- resources/views/services/report_user.blade.php
+++ resources/views/services/report_user.blade.php
@@ -19,7 +19,7 @@
             @foreach($row['data'] as $data)
                 <div style="display: flex; position: relative">
                     <i style="transform: rotate(90deg); float: left; margin: 5px 15px 5px 30px" class="fas fa-level-up-alt"></i>
-                    <div style="float: left; width: 150px; text-align: center; margin-right: 15px" class="<?php echo \App\Utils\StatusUltil::getStatus($data->status_id)['class']; ?>">
+                    <div style="float: left; width: 150px; text-align: center; margin-right: 15px" class="<?php echo \App\Utils\StatusUltil::getStatus($data->status_id, true)['class']; ?>">
                         {!! $data->status !!}
                     </div>
                     <span><b>Quantidade:</b> {!! $data->total !!}</span>
